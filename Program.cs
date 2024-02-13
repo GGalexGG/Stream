82c07cde24324e123dbecba95109b6a53a5097fe
Starlyzy <sedinin2017@yandex.ru>
2024-02-13 17:13:28 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿static class Program
+{
+    //Вывод сообщения из файла
+    //Вывод сообщения введите свое ФИО
+    //Считывание ФИО
+    //В файл дополняешь фио
+
+    static void Main()
+    {
+         string UserName = Console.ReadLine();
+        string path = "C:\\Users\\user\\LoremIpsum\\text.txt";      
+        try 
+        {
+            var sr = new StreamReader(path);
+            Console.WriteLine(sr.ReadToEnd());   
+        }       
+        catch (Exception e)
+        {
+           Console.WriteLine("The file could not be read:"  + e.Message);
+        }       
+    }
+}
